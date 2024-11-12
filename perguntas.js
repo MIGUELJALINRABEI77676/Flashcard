1bab6a2b459b79c9e2759412b50fc41724413dac
MIGUELJALINRABEI77676 <00001117353424sp@al.educacao.sp.gov.br>
2024-11-12 20:04:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Academia',
+    'Qual é a importância do aquecimento antes do treino?',
+    'O aquecimento prepara o corpo, reduz o risco de lesões e melhora o desempenho durante o treino.'
+)
+
+criaCartao(
+    'Academia',
+    'O que é um treino HIIT?',
+    'HIIT (High-Intensity Interval Training) é um tipo de treino que alterna períodos curtos de exercícios intensos com pausas, aumentando a queima de calorias em pouco tempo.'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é o artilheiro da Copa do Mundo de todos os tempos?',
+    'Marta é a artilheira da Copa do Mundo feminina, e Miroslav Klose é o artilheiro da Copa do Mundo masculina de todos os tempos.'
+)
+
+criaCartao(
+    'Futebol',
+    'O que significa o termo “hat-trick” no futebol?',
+    'Hat-trick é quando um jogador marca três gols em uma única partida.'
+)
